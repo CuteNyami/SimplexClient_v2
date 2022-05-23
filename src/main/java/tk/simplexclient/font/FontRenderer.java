7efedf9f93a0f5f9042b82dc4561cd9e02f6884d
Nyami <96192624+CuteNyami@users.noreply.github.com>
2022-05-23 18:54:16 +0200

    Update FontRenderer.java

--- src/main/java/tk/simplexclient/font/FontRenderer.java
+++ src/main/java/tk/simplexclient/font/FontRenderer.java
@@ -18,10 +18,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-/**
- * This class was not coded by any of the SimplexClient developers!
+/*
+ *       Copyright (C) 2018-present Hyperium <https://hyperium.cc/>
  *
- * @author Unknown
+ *       This program is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published
+ *       by the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 @SuppressWarnings("all")
 public class FontRenderer
